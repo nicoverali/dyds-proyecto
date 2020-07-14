1e65a35888bc6513963b2c2fd53efcf33ca93bb9
franco1fs <sacomanif@gmail.com>
2020-07-14 13:09:26 -0300

    WordCacheSQLiteTest updated

--- src/test/java/model/cache/WordCacheSQLiteTest.java
+++ src/test/java/model/cache/WordCacheSQLiteTest.java
@@ -44,60 +44,40 @@ class WordCacheSQLiteTest {
     }
 
     @BeforeEach
-    public void createTable_shouldBeCallAlways_WhenCreateAWordCacheSQLiteObj(){
-        try {
+    public void createTable_shouldBeCallAlways_WhenCreateAWordCacheSQLiteObj() throws SQLException{
 
             verify(sqLiteDBMock).createTable(TABLE_NAME,TABLE_ATTRIBUTES);
 
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
     }
-    @DisplayName("Save Word into 'terms' table de sqlite")
+
+
+    @DisplayName("Save Word into 'words' table de sqlite with exactly the same sql sentence")
     @Test
-    public void saveWord_shouldCallSQLiteDB_WithExecuteUpdate(){
+    public void saveWord_shouldCallSQLiteDB_WithExecuteUpdate() throws SQLException{
 
         wordCacheSQLite_test.save(word);
 
-
-        try {
-
-            verify(sqLiteDBMock).executeUpdate
+        verify(sqLiteDBMock).executeUpdate
                     ("INSERT INTO words VALUES( NULL, \""+word.getTerm()+"\", \""+word.getMeaning()+"\", 1, CURRENT_TIMESTAMP )");
-
-
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-
 
     }
 
+    @DisplayName("getWord should query db with the same query and no find term, so return word null")
     @Test
-    public void getWord_ShouldQueryDB_getResultset_AndGetWordForm(){
+    public void getWord_ShouldQueryDB_getResultsetEmpty_AndGetWordNull() throws SQLException{
 
         String term = "test";
 
-        try {
-            when(sqLiteDBMock.executeQuery(any())).thenReturn(resultSetMock);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+        when(sqLiteDBMock.executeQuery(any())).thenReturn(resultSetMock);
 
-        try {
-            wordCacheSQLite_test.get(term);
+        Word wordAnswer = wordCacheSQLite_test.get(term);
 
-            String query = "SELECT * FROM "+TABLE_NAME+" WHERE term == "+term+" AND date >= DATE('now', '-1 day')";
+        String query = "SELECT * FROM "+TABLE_NAME+" WHERE term == "+term+" AND date >= DATE('now', '-1 day')";
 
-            verify(sqLiteDBMock).executeQuery(query);
+         verify(sqLiteDBMock).executeQuery(query);
 
-            //verify(when(sqLiteDBMock.executeQuery(query)).thenReturn(resultSetMock));
+         assertEquals(null,wordAnswer);
 
-
-
-        } catch (SQLException throwables) {
-            System.out.println(throwables.getMessage());
-        }
 
     }
 }
